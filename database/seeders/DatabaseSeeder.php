8f8cac89e4c9a202374dc8322359057e69fed39b
wafaaBasil96 <wafaabasil1996@gmail.com>
2024-06-30 21:38:33 +0300

    29-6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        $this->call(PaymentMethodSeeder::class);
+       /* $this->call(PaymentMethodSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(ProviderSeeder::class);
         $this->call(OrderSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ProviderCategorySeeder::class);
-        $this->call(RatingSeeder::class);
+        $this->call(RatingSeeder::class);*/
+        $this->call(CouponSeeder::class);
         /*
         User::factory()->create([
             'name' => 'Test User',
