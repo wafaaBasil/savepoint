c596605e9c818521fb49b856df52eb4fd1a0c2e8
wafaaBasil96 <wafaabasil1996@gmail.com>
2024-07-08 13:55:11 +0300

    29-6

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,7 +24,7 @@ class Product extends Model
 
     public function category(): BelongsTo
     {
-        return $this->belongsTo(ProductCategory::class);
+        return $this->belongsTo(ProductCategory::class,'category_id');
     }
 
     public function enhancements(): BelongsToMany
