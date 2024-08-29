a478947831816b107bb96bf0033454353f53d1de
wafaaBasil96 <wafaabasil1996@gmail.com>
2024-08-29 21:42:12 +0300

    24_8

--- app/Http/Controllers/API/ProviderDashboard/ReportController.php
+++ app/Http/Controllers/API/ProviderDashboard/ReportController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Branch;
 use App\Models\Order;
 use App\Models\Provider;
+use Carbon\Carbon;
 use App\Models\User;
 use App\Http\Resources\User as UserResource;
 use App\Http\Resources\Branch as BranchResource;
@@ -61,6 +62,21 @@ class ReportController extends BaseController
         $success['profits_total']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->sum('order_price');
         $success['revenues_total']=1111;
         $success['expenses_total']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->sum('order_price');
+
+        $currentYear = Carbon::now()->year;
+
+        $success['january']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '1')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['february']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '2')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['march']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '3')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['april']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '4')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['may']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '5')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['june']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '6')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['july']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '7')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['august']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '8')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['september']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '9')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['october']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '10')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['november']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '11')->whereYear('created_at', $currentYear)->sum('order_price');
+        $success['december']=Order::where('provider_id',auth("sanctum")->user()->provider_id)->whereMonth('created_at', '12')->whereYear('created_at', $currentYear)->sum('order_price');
 
         $success['status']= 200;
 
