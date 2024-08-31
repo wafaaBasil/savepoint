1786ef7bc15e344b28700daf8eff50ace2c25628
wafaaBasil96 <wafaabasil1996@gmail.com>
2024-08-31 16:03:48 +0300

    24_8

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use Carbon\Carbon;
 class Branch extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     public function city(): BelongsTo
     {
         return $this->belongsTo(City::class);
