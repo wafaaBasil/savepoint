3f26e57b7ab6cead7c69a7353ac63e52d5aad8ae
wafaaBasil96 <wafaabasil1996@gmail.com>
2024-09-02 16:09:59 +0300

    24_8

--- app/Http/Resources/Day.php
+++ app/Http/Resources/Day.php
@@ -15,6 +15,7 @@ class Day extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'=>$this->id,
             'name_ar' => $this->name_ar,
             'name_en' => $this->name_en,
         ];
